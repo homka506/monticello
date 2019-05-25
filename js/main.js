c60ce746c14fe018433bd0c43009a8346cee64c8
unknown <polina.kalashnikova1985@gmail.com>
2019-05-25 15:36:04 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,14 @@
+;(function() {
+    "use strict";
+    
+let headingSlider = $(".ba-slider__header");
+headingSlider.slick({
+    arrows:false,
+    dots:false,
+    infinite: true,
+
+});
+
+})();
+
+
